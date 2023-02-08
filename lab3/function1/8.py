84d39e349d07c28a3bbd6e3a856a679444f9e145
DiyasKD <123235687+DiyasKD@users.noreply.github.com>
2023-02-09 00:29:24 +0600

    Update 8.py

--- lab3/function1/8.py
+++ lab3/function1/8.py
@@ -1,11 +1,13 @@
 #Write a function that takes in a list of integers and returns True if it contains 007 in order
 def find_007(a):
   for i in range(len(a)):
-    if a[i] == 0 and a[i+1] == 0 and a[i+2] == 7:
-      return True
+    if a[i] == 0:
+      for j in range(i + 1, len(a)):
+        if a[j] == 0:
+          for h in range(j + 1, len(a)):
+            if a[h] == 7:
+              return True
   return False
 
 a= [int(x) for x in input().split()]
 print(find_007(a))
-
-
