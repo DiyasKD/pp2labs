a008f73a35c99e69c1f978eaceeb0f2dea3e4561
DiyasKD <123235687+DiyasKD@users.noreply.github.com>
2023-02-09 00:28:50 +0600

    Update 7.py

--- lab3/function1/7.py
+++ lab3/function1/7.py
@@ -1,6 +1,6 @@
 # Given a list of ints, return True if the array contains a 3 next to a 3 somewhere.
 def find_33(a):
-    for i in range(len(a)):
+    for i in range(len(a) - 1):
         if a[i] == 3 and a[i+1] == 3:
             return True
     return False
