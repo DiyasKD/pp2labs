9b0901fb7e1fd93d00e1ee16257fd3bd7b37f0d8
DiyasKD <123235687+DiyasKD@users.noreply.github.com>
2023-03-05 22:31:08 +0600

    Update 5.py

--- lab6/dir_and_files/5.py
+++ lab6/dir_and_files/5.py
@@ -1,5 +1,6 @@
 filename = input("Enter a filename: ")
-list_to_write = ['apple', 'banana', 'cherry', 'date']
+s = str(input())
+list_to_write = s.split()
 
 with open(filename, 'w') as file:
     for item in list_to_write:
