3ac721f1802181ee5b017d555b56300baecf970f
DiyasKD <123235687+DiyasKD@users.noreply.github.com>
2023-03-06 23:38:33 +0600

    Update 4.py

--- lab6/func/4.py
+++ lab6/func/4.py
@@ -2,7 +2,7 @@ from time import sleep
 import math
 t = int(input())
 n = int(input())
-def invoke_sleeping(num, ms, *args):
+def invoke_sleeping(num, ms):
     sleep(ms/1000)
-    return num(*args)
-print("Square root of", n , "after", t ,"specific miliseconds is", invoke_sleeping(lambda x: math.sqrt(x), t, n)) 
+    return math.sqrt(num)
+print("Square root of", n , "after", t ,"specific miliseconds is", invoke_sleeping(n, t)) 
